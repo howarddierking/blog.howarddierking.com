cda6fb8e70e60257942d495d5d600243c36e28cd
Howard Dierking <howarddierking@gmail.com>
2016-09-05 22:20:25 -0700

    at feature parity for the web site. Need to add RSS and site mapping

--- build.js
+++ build.js
@@ -5,34 +5,56 @@ const permalinks = require('metalsmith-permalinks');
 const layouts = require('metalsmith-layouts');
 const serve = require('metalsmith-serve');
 const watch = require('metalsmith-watch');
-const dateInFileName = require('metalsmith-date-in-filename')
+const dateInFileName = require('metalsmith-date-in-filename');
+const collections = require('metalsmith-collections');
+const moment = require('moment');
+const excerpts = require('metalsmith-excerpts');
+const pagination = require('metalsmith-pagination');
 
-let build = metalsmith(__dirname)
+var build = metalsmith(__dirname)
   .metadata({
     site: {
       title: 'blog.howarddierking.com',
       url: 'http://blog.howarddierking.com'
     }
   })
-  .use(drafts())
   .use(markdown())
+  .use(excerpts())
   .use(dateInFileName({
     override: true
+  }))
+  .use(collections({
+    blogposts: {
+      pattern: 'posts/*.html',
+      reverse: true 
+    }
   }))
   .use(permalinks({
     pattern: ':date/:title'
   }))
+  .use(pagination({
+    'blogposts': {
+      perPage: 5,
+      layout: 'homepage.html',
+      first: 'index.html',
+      path: 'blog/page:num/index.html',
+      pageMetadata: {
+        title: 'Archive'
+      }
+    }
+  }))
   .use(layouts({
     engine: 'qejs',
-    directory: 'layouts'
+    directory: 'layouts',
+    moment: moment  // <-- the ability to pass libraries to the template engine may be the best feature
   }))
-  // .use(serve({
-  //   verbose: true
-  // }))
-  // .use(watch({
-  //   pattern: '**/*',
-  //   livereload: true
-  // }))
+  .use(serve({
+    verbose: true
+  }))
+  .use(watch({
+    pattern: '**/*',
+    livereload: true
+  }))
   .source('./src')
   .destination('./site')
   .build(err => {
